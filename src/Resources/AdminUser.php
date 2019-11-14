cf730ed2855d5793ede93f55cfbc207a3bb0399f
osi <yaoiluo@gmail.com>
2019-11-14 11:46:07 +0800

    :art: resource add attrs meta for response

--- src/Resources/AdminUser.php
+++ src/Resources/AdminUser.php
@@ -2,19 +2,22 @@
 
 namespace Moell\Mojito\Resources;
 
-
 use Illuminate\Http\Resources\Json\Resource;
 
 class AdminUser extends Resource
 {
     public function toArray($request)
     {
-        return [
-            'id'    => $this->id,
-            'name'  => $this->name,
-            'email' => $this->email,
-            'created_at' => (string)$this->created_at,
-            'updated_at' => (string)$this->updated_at
-        ];
+        if ($this->resourceAttrs) {
+            return array_only($this->resource->toArray(), $this->resourceAttrs);
+        } else {
+            return [
+                'id' => $this->id,
+                'name' => $this->name,
+                'email' => $this->email,
+                'created_at' => (string) $this->created_at,
+                'updated_at' => (string) $this->updated_at,
+            ];
+        }
     }
 }
